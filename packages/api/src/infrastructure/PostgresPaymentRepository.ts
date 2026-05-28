d7c3d9d7cfba8fc2857848ce8ed43c1824d7e8c3
Dana Natasha Cadabon <natashacadabon@gmail.com>
2026-05-28 20:27:19 -0300

    chore(test): configura prisma y postgres para e2e

--- packages/api/src/infrastructure/PostgresPaymentRepository.ts
+++ packages/api/src/infrastructure/PostgresPaymentRepository.ts
@@ -9,7 +9,7 @@ if (!process.env.DATABASE_URL) {
 }
 
 const prisma = new PrismaClient({
-    adapter: new PrismaPg(process.env.DATABASE_URL),
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
 });
 
 type DBPayment = {
